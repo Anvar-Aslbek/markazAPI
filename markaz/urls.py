2cddb3086cea221f34ddaa10c184ac8c2d0dd747
AnvarNarzullayev <anvarnarzullayev12@gmail.com>
2022-06-22 11:39:53 +0500

    bir nechta uzgartirishlar qushildi

--- markaz/urls.py
+++ markaz/urls.py
@@ -2,6 +2,17 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', StudentAPIView.as_view()),
-    path('detail/student/<int:pk>', StudentDetailAPIViews.as_view())
+    path('student/', StudentAPIView.as_view()),
+    path('detail/student/<int:pk>', StudentDetailAPIViews.as_view()),
+    path('', StudentListApiView.as_view()),
+    path('xodim/', EmployeeListApiView.as_view()),
+    path('course/', CourseListApiView.as_view()),
+    path('day/', DayListApiView.as_view()),
+    path('course/', CourseListApiView.as_view()),
+    path('course/', CourseListApiView.as_view()),
+    path('course/', CourseListApiView.as_view()),
+    path('course/', CourseListApiView.as_view()),
+    path('course/', CourseListApiView.as_view())
+
+
 ]
--- markaz/views.py
+++ markaz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
@@ -8,6 +9,15 @@ from .serializers import *
 from .models import *
 
 # Create your views here.
+class StudentListApiView(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = StudentSerializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class StudentAPIView(APIView):
     def get(self, request):
@@ -52,5 +62,113 @@ class StudentDetailAPIViews(APIView):
 
 
 
+class EmployeeListApiView(ListAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = XodimSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = XodimSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class CourseListApiView(ListAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = CourseSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class DayListApiView(ListAPIView):
+    queryset = Day.objects.all()
+    serializer_class = DaySerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = DaySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class XarajatListApiView(ListAPIView):
+    queryset = Xarajat.objects.all()
+    serializer_class = XarajatSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = XarajatSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class HarajatTuriListApiView(ListAPIView):
+    queryset = Harajatturi.objects.all()
+    serializer_class = HarajatturiSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = HarajatturiSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class IshHaqqiTuriListApiView(ListAPIView):
+    queryset = IshHaqqiTuri.objects.all()
+    serializer_class = IshHaqqiTuriSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = IshHaqqiTuriSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class IshHaqqiListApiView(ListAPIView):
+    queryset = IshHaqqi.objects.all()
+    serializer_class = IshHaqqiSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = IshHaqqiSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class PaymentListApiView(ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = PaymentSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class RollListApiView(ListAPIView):
+    queryset = Roll.objects.all()
+    serializer_class = RollSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = RollSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class RoomListApiView(ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = RoomSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class XarajatListApiView(ListAPIView):
+    queryset = Xarajat.objects.all()
+    serializer_class = XarajatSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = XarajatSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
